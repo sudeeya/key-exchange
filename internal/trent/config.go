b6174ae95ef99d09d0f2f875bb7e20745d05368a
sudeeya <pyrkin.aa@phystech.edu>
2024-09-25 15:22:59 +0300

    Change agent config and add trent config

--- internal/trent/config.go
+++ internal/trent/config.go
@@ -1,15 +1,13 @@
-package agent
+package trent
 
 import "github.com/caarlos0/env"
 
 type config struct {
-	ID         string `env:"ID,required"`
 	PublicKey  string `env:"PUBLIC_KEY,required"`
 	PrivateKey string `env:"PRIVATE_KEY,required"`
 
-	TrentAddr  string   `env:"TRENT_ADDR,required"`
-	AgentIDs   []string `env:"AGENT_IDS,required"`
-	AgentAddrs []string `env:"AGENT_ADDRS,required"`
+	ClientIDs        []string `env:"CLIENT_IDS,required"`
+	ClientPublicKeys []string `env:"CLIENT_PUBLIC_KEYS,required"`
 }
 
 func NewConfig() (*config, error) {
