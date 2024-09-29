5752c699c71219c8ee6134777176afe33183b591
sudeeya <pyrkin.aa@phystech.edu>
2024-09-29 21:14:27 +0300

    Add rng package

---
+++ internal/pkg/rng/rng.go
@@ -0,0 +1,32 @@
+package rng
+
+import (
+	"crypto/rand"
+)
+
+const (
+	KuznyechikKeySize = 32
+	NonceSize         = 16
+)
+
+type RNG struct{}
+
+func NewRNG() *RNG {
+	return &RNG{}
+}
+
+func (rng RNG) GenerateNonce() ([]byte, error) {
+	key := make([]byte, NonceSize)
+	if _, err := rand.Reader.Read(key); err != nil {
+		return nil, err
+	}
+	return key, nil
+}
+
+func (rng RNG) GenerateKey(bytes int) ([]byte, error) {
+	key := make([]byte, bytes)
+	if _, err := rand.Reader.Read(key); err != nil {
+		return nil, err
+	}
+	return key, nil
+}
